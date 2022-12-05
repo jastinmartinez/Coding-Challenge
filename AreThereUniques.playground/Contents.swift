27cd4d289b2245895c07e0940d67fe09c2ce4e8b
Jastin Martinez <martinez.jastin@hotmail.com>
2022-12-05 12:45:02 -0400

    Challenger No. 1 - Are Letters Unique

--- AreThereUniques.playground/Contents.swift
+++ AreThereUniques.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Foundation
 import XCTest
 
+func areTheLettersUnique(_ letters: String) -> Bool {
+    var characterDictionary = [Character: Bool]()
+    for character in letters {
+        if let isUnique = characterDictionary[character] {
+            return !isUnique
+        } else {
+            characterDictionary[character] = true
+        }
+    }
+    return true
+}
+
+class AreTheLetterUniqueTests: XCTestCase {
+    func test_IsUnique() {
+        assert(areTheLettersUnique("No duplicates") == true, "failed 1 tets")
+        assert(areTheLettersUnique("abcdefghijklmnopqrstuvwxyz") == true, "failed 2 tets")
+        assert(areTheLettersUnique("AaBbCc") == true, "failed 3 tets")
+        assert(areTheLettersUnique("Hello, world") == false, "failed 4 tets")
+    }
+}
+
+AreTheLetterUniqueTests.defaultTestSuite.run()
