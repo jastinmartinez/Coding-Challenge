39e33d5f331da4552566842ecb440d33202b2b88
Jastin Martinez <martinez.jastin@hotmail.com>
2023-07-10 20:27:22 -0400

    isMonolitic

--- array/isMonotonic.playground/Contents.swift
+++ array/isMonotonic.playground/Contents.swift
@@ -1,6 +1,34 @@
 import UIKit
 
 func isMonotonic(array: [Int]) -> Bool {
-    // Write your code here.
-    return false
-  }
+    var index = 0
+    var isIncreasing: Bool? = nil
+    while (index + 1) < array.count {
+        if array[index + 1] == array[index] {
+            index += 1
+            continue
+        } else {
+            if array[index + 1] < array[index] {
+                if isIncreasing == nil {
+                    isIncreasing = false
+                } else {
+                    if let isIncreasing = isIncreasing, isIncreasing {
+                        return false
+                    }
+                }
+            } else {
+                if isIncreasing == nil {
+                    isIncreasing = true
+                } else {
+                    if let isIncreasing = isIncreasing, !isIncreasing {
+                        return false
+                    }
+                }
+            }
+        }
+        index += 1
+    }
+    return true
+}
+var array = [1,2,3,3,2,1]
+isMonotonic(array: array)
