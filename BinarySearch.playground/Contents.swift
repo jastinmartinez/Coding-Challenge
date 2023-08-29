196ab07e2c64f97e412cdb6737009b1770255b92
Jastin Martinez <martinez.jastin@hotmail.com>
2023-08-29 10:16:45 -0400

    refactor to realy used binary search approach

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -1,27 +1,26 @@
 import UIKit
 
-var array = [5, 23, 111]
-let target = 111
+var array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73, 355]
+let target = 21
 
 func binarySearch(array: [Int], target: Int) -> Int {
     var lhs = 0
     var lhr = array.count-1
-    var index = lhr - lhs
+    var index = (array.count-1) / 2
     while lhs != lhr {
         guard index > -1 && index < array.count else {
             return -1
         }
         let value = array[lhs...lhr][index]
         if value > target {
-            index -= 1
-            lhr = index
+            lhr = index - 1
         } else if value < target  {
-            index += 1
-            lhs = index
+            lhs = index + 1
         } else {
             return index
         }
-        index = lhr - lhs
+        
+        index = (lhr + lhs) / 2
     }
     return array[index] == target ? index : -1
 }
