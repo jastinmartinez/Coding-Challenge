84ba3c8befab11ef15887b29d7645c23b36db255
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-25 20:46:59 -0400

    fix an issue doesn't work for all

--- SpitalTraverse.playground/Contents.swift
+++ SpitalTraverse.playground/Contents.swift
@@ -4,34 +4,37 @@ func spiralTraverse(array: [[Int]]) -> [Int] {
     var result = [Int]()
     var startRow = 0, endRow = array.count - 1
     var startCol = 0, endCol = array[0].count - 1
-    
     while startRow <= endRow && startCol <= endCol {
-        for col in startCol..<endCol + 1 {
+        for col in stride(from: startCol, to: endCol + 1, by: 1) {
             result.append(array[startRow][col])
         }
-        
-        for row in startRow..<endRow + 1 {
-            //            print(1)
+        startRow += 1
+        for row in stride(from: startRow, to: endRow + 1, by: 1){
             result.append(array[row][endCol])
         }
-        
-        for col in stride(from: endCol, to: startCol, by: -1) {
-            print(1)
-            result.append(array[endRow][col])
-        }
-        
-        for row in stride(from: endRow, to: startRow, by: -1) {
-            print(1)
-            result.append(array[row][startCol])
-        }
-        
-        startRow += 1
-        endRow -= 1
-        startCol += 1
         endCol -= 1
+        if startRow <= endRow {
+            for col in stride(from: endCol, to: startCol - 1, by: -1) {
+                result.append(array[endRow][col])
+            }
+        }
+        endRow -= 1
+        if startCol <= endCol {
+            for row in stride(from: endRow, to: startRow - 1, by: -1) {
+                result.append(array[row][startCol])
+            }
+        }
+        startCol += 1
     }
     return result
 }
-var array =  [[1, 3, 2, 5, 4, 7, 6]]
+var array =  [[1, 2, 3, 4],
+              [12, 13, 14, 5],
+              [11, 16, 15, 6],
+              [10, 9, 8, 7]]
 
-print(spiralTraverse(array: array))
+var array1 =  [[1, 2, 3, 4],
+               [10, 11, 12, 5],
+               [9, 8, 7, 6]]
+
+print(spiralTraverse(array:  array))
